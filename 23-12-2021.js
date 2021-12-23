fd9b16762f2380e4d15d5978e5102cbd10256723
Ramish Qamar <95705163+MianRamish@users.noreply.github.com>
2021-12-23 04:19:54 -0800

    Add files via upload

---
+++ 23-12-2021.js
@@ -0,0 +1,13 @@
+function hideButton() {
+    document.getElementById("hideContent").style.display = "none";
+}
+function showButton() {
+    document.getElementById("hideContent").style.display = "block";
+}
+ var x = document.getElementById("input1").value;
+ document.getElementById("inputdata").innerHTML = x;
+
+
+ function reset() {
+     document.getElementById("form1").reset;
+ }
